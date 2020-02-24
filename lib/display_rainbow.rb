6b664a4f8eb7cdad08468c561de28717028eaf93
Marlon Howard <mjhow4@gmail.com>
2020-02-24 20:02:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,4 @@ def display_rainbow(colors)
 puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{color[5]}, V: #{colors[5]}"
 #  colors.each {|color| print "#{color[0].capitalize}: #{color}, "}
 end
-display_rainbow(colors)
+
